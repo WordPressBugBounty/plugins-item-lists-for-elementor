84a9dc831b5d7e6d1982668e4bbcc5f6890afdcb
Sean Murphy <sean@iamseanmurphy.com>
2024-10-03 06:43:24 +0000

    1.3.6

---
+++ item-lists-for-elementor/include/item-lists-element/alternate-timeline-bullet-style.php
@@ -0,0 +1,31 @@
+<!-- Start Alternate Timeline Bullet Style -->
+<div class="ile-alternate-timeline-bullet-style">
+    <?php $count = 0; 
+    foreach ($settings['timeline_item_lists'] as $items => $item) {
+        $icon = $item['timeline_list_items_icon']['value'];
+        $title = $item['timeline_list_items_title'];
+        $content = $item['timeline_list_items_content'];
+        $icon_color = $item['timeline_icon_color'];
+        $icon_bg_color = $item['timeline_icon_bg_color'];
+        $ile_timeline_content = "ile-timeline-right";
+        if($count % 2 == 0) { 
+            $ile_timeline_content = "ile-timeline-left";  
+        } ?>
+        <div class="ile-container-holder <?php echo esc_attr($ile_timeline_content); ?>">
+            <div class="ile-timeline-vertical"></div>
+            <div class="ile-timeline-horizontal"></div>
+            <?php if($item['timeline_list_items_display_icon'] === 'icon') { ?>
+            <div class="ile-icon-box" style="background-color:<?php echo esc_attr($icon_bg_color); ?>">
+                <i class="ile-icon <?php echo esc_attr($icon) ?>" style="color:<?php echo esc_attr($icon_color); ?>;"></i>
+            </div>
+            <?php } else if($item['timeline_list_items_display_icon'] === 'image') { ?>
+                <img src="<?php echo esc_url($item['timeline_list_items_image']['url']); ?>" class="ile-image" /><?php
+            } ?>
+            <div class="ile-content-box">
+                <h2 class="ile-title"><?php echo esc_attr($title); ?></h2>
+                <p class="ile-content"><?php echo wp_kses_post($content); ?></p>
+            </div>
+        </div>
+        <?php $count = $count +1; } ?>
+</div>
+<!-- End Alternate Timeline Bullet Style -->
